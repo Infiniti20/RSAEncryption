0c7c3ad789b6592b2a913e2299f97b2daf738789
Infiniti20 <77065635+Infiniti20@users.noreply.github.com>
2021-01-06 11:42:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import rsaEncryption as rsa
-e=rsa.RSAEncryption()
-# print(e.createKeys())
-l=e.encrypt("It's finally working",[139,185])
-print(l)
-l=e.decrypt(l,[115,185])
-print()
-print(l)
+RSA=rsa.RSAEncryption()
+KEYS=RSA.createKeys()
+PUB_KEY=KEYS[0]
+PRIV_KEY=KEYS[1]
+
+ciphertext=
 
