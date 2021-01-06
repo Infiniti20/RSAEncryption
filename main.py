bb40d3688848652f59ce9bef9792981cf8f69dd3
Infiniti20 <77065635+Infiniti20@users.noreply.github.com>
2021-01-06 11:46:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import rsaEncryption as rsa
 RSA=rsa.RSAEncryption()
-KEYS=RSA.createKeys()
+KEYS=RSA.CREATE_KEYS()
 PUB_KEY=KEYS[0]
 PRIV_KEY=KEYS[1]
 
-ciphertext=
+plaintext=input("Text to hash: ")
+ciphertext=RSA.ENCRYPT(plaintext,PUB_KEY)
+print("Encrypted Text: ",("").join(ciphertext))
+decryptedtext=RSA.DECRYPT(ciphertext,PRIV_KEY)
+print("\nDecrypted Text:", decryptedtext)
 
